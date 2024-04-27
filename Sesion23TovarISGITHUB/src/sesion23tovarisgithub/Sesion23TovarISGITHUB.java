d505047b3ef17653c9394bae9dc18a434a9ef2ce
ttovarra123 <71203987+ttovarra123@users.noreply.github.com>
2024-04-26 21:39:03 -0600

    Confirmacion Tovar 1
    OK

---
+++ Sesion23TovarISGITHUB/src/sesion23tovarisgithub/Sesion23TovarISGITHUB.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sesion23tovarisgithub;
+
+/**
+ *
+ * @author HyFlex
+ */
+public class Sesion23TovarISGITHUB {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+          System.out.println("Prueba de GITHUB 1 TOVAR");
+    }
+    
+}
